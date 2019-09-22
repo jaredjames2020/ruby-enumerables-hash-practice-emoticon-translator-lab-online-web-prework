9804659c82db179a3b34ea2e976012e0bb2a9d0f
Jared S <jaredjames2020@gmail.com>
2019-09-22 19:00:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,11 +23,11 @@ def get_japanese_emoticon(file_path, english_emoticon)
   #code goes here
   emotions = load_library(file_path)
   emotions.each do |key, v|
-    #binding.pry
-    # if key[v].include == (english_emoticon)
-    #   v.value
-    # binding.pry
-    # end
+    binding.pry
+    if key[v].include == (english_emoticon)
+      v.value
+    binding.pry
+    end
   end
     #binding.pry
   #use value to ID key and return 2 value
