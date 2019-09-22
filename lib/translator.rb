0324e5aae68a8a759ee75630de11538c183ae443
Jared S <jaredjames2020@gmail.com>
2019-09-22 19:02:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
     if key[v].include == (english_emoticon)
       #binding.pry
       v.value
-    binding.pry
+    #binding.pry
     end
   end
 
