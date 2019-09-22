52c8e51957f9272cb7f107142e1453ed49a26e4e
Jared S <jaredjames2020@gmail.com>
2019-09-22 19:20:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
      return emotions[:get_emoticon][english_emoticon]
    end
   end
-  "Sorry"
+  "Sorry, that emoticon was not found"
   #emotions[:get_emoticon][english_emoticon]
 end
 
