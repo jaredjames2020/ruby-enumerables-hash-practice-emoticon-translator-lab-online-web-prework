347f6491fe12ba9adad222708d00e1b4097079f3
Jared S <jaredjames2020@gmail.com>
2019-09-22 18:55:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,6 +28,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
       v.value
     #binding.pry
     end
+  end
     #binding.pry
   #use value to ID key and return 2 value
 
