1ec9ba3242f0fe0c87d7f868651393076dd79310
Jared S <jaredjames2020@gmail.com>
2019-09-22 19:14:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
   emotions = load_library(file_path)
   emotions[:get_emoticon].each do |k,v| 
     if k == english_emoticon
-      return v
+      v
     #emotions[:get_emoticon][english_emoticon]
     #binding.pry
    end
